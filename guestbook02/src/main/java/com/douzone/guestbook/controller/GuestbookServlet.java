afa0e8fa12898c72fb08aa6edf597e72468382eb
Parkyeongmi98 <yeongmi_@naver.com>
2023-01-17 16:35:13 +0900

    guestbook02 #1

--- guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
+++ guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookServlet.java
@@ -10,11 +10,13 @@ public class GuestbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
